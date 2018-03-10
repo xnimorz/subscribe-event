7b15face6fa4cfc5253e91165a70c699a6c4f942
Nikita Mostovoy <nik.mostovoy@gmail.com>
2018-03-10 18:14:25 +0300

    decrease package size

--- subscribe.js
+++ subscribe.js
@@ -1,40 +1,28 @@
 !(function() {
   "use strict";
 
-  function subscribeEvent(
-    element,
-    event,
-    handler,
-    options,
-    addSubscription,
-    removeSubscription
-  ) {
-    var unsubscribeFunction = removeSubscription;
+  // Basic subscribe function both browser events or node.js events
+  function subscribeEvent(element, event, handler, options) {
+    var unsubscribeFunctionName = "removeEventListener";
     var unsubscribe = function() {
-      element[unsubscribeFunction](event, handler);
+      element[unsubscribeFunctionName](event, handler);
     };
-
-    if (addSubscription) {
-      element[addSubscription](event, handler, options);
-      return unsubscribe;
-    }
 
     if (element.addEventListener) {
       element.addEventListener(event, handler, options);
-      unsubscribeFunction = "removeEventListener";
       return unsubscribe;
     }
 
     if (element.attachEvent) {
       event = "on" + event;
       element.attachEvent(event, handler);
-      unsubscribeFunction = "detachEvent";
+      unsubscribeFunctionName = "detachEvent";
       return unsubscribe;
     }
 
     if (element.on) {
       element.on(event, handler);
-      unsubscribeFunction = "off";
+      unsubscribeFunctionName = "off";
       return unsubscribe;
     }
   }
@@ -43,29 +31,27 @@
     return subscribeEvent(element, event, handler, options);
   }
 
+  // define your own subscribe functions
   subscribe.define = function(addSubscription, removeSubscription) {
     return function(element, event, handler, options) {
-      return subscribeEvent(
-        element,
-        event,
-        handler,
-        options,
-        addSubscription,
-        removeSubscription
-      );
+      var unsubscribe = function() {
+        element[removeSubscription](event, handler);
+      };
+
+      element[addSubscription](event, handler, options);
+      return unsubscribe;
     };
   };
 
-  (function(subscribe) {
-    if (typeof exports === "object") {
-      // CommonJS
-      module.exports = subscribe;
-    } else if (typeof define === "function" && define.amd) {
-      // AMD. anonymous module
-      define(subscribe);
-    } else {
-      // Global scope
-      global.subscribeEvent = subscribe;
-    }
-  })(subscribe);
+  // umd
+  if (typeof exports === "object") {
+    // CommonJS
+    module.exports = subscribe;
+  } else if (typeof define === "function" && define.amd) {
+    // AMD. anonymous module
+    define(subscribe);
+  } else {
+    // Global scope
+    global.subscribeEvent = subscribe;
+  }
 })();
